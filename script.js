83681a0e0c3d6e1f911f35c3102690cf201ebec8
Stephen Vizzusi <svizzusi13@gmail.com>
2023-01-21 18:09:29 -0500

    Added a java prompt

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+let user = window.prompt("What is your name?");
+h2.textContent = `Hello, ${user}`;
 document.querySelector("body").appendChild(h2);
+
