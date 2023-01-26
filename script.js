24face2ae92cf60c8955aa184c6bb8b164da795f
Stephen Vizzusi <svizzusi13@gmail.com>
2023-01-26 13:24:05 -0500

    background and nav bars positioned

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const h2 = document.createElement("h2");
-let user = window.prompt("What is your name?");
-h2.textContent = `Hello, ${user}`;
-document.querySelector("body").appendChild(h2);
+h2.textContent = "Welcome to the Jungle";
+document.querySelector("header").appendChild(h2);
+
+
+
 
